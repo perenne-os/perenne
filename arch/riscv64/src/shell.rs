ffd9c5f094d029f4a26dcb17ebffa22c86541e81
Kathir <kathirpsmy@gmail.com>
2026-06-27 14:01:01 -0400

    feat(shell): kb command shows each entry's seen counter

--- arch/riscv64/src/shell.rs
+++ arch/riscv64/src/shell.rs
@@ -102,8 +102,8 @@ fn dispatch(cmd: &str) {
         "help" => crate::println!("commands: help, kb, diag"),
         "kb" => {
             let mut i = 0;
-            while let Some((id, title)) = crate::heal::entry(i) {
-                crate::println!("{id}  {title}");
+            while let Some((id, title, seen)) = crate::heal::entry(i) {
+                crate::println!("{id} (seen {seen})  {title}");
                 i += 1;
             }
             if i == 0 {
